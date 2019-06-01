c74f30cf98a1abe7f433db7cfee18016f8d99204
Jhordan Figueroa <jhordan.a.figueroa@gmail.com>
2019-05-31 20:51:38 -1000

    create game logic objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,43 @@
 console.log("Aloha World!");
-var cards = ["queen", "ace", "king", "joker"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png",
+	}
+];
 var cardsInPlay = []; 
 
 function checkForMatch () {
 	if (cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You've found a match!"); 
+		alert("You've found a match!"); 
 	} else {
-		console.log("Sorry, try again!");
+		alert("Sorry, try again!");
 	}
 }
 
 function flipCard (cardID) {
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
 	checkForMatch ();
-}
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
+};
 
 flipCard(0);
 flipCard(2);
