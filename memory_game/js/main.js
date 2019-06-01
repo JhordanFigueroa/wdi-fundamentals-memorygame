6a40ada68ce2590d5945244d54857e898977eb9c
Jhordan Figueroa <jhordan.a.figueroa@gmail.com>
2019-05-31 19:10:05 -1000

    created game logic with functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,21 @@
 console.log("Aloha World!");
 var cards = ["queen", "ace", "king", "joker"];
-var cardsInPlay = [, ,]; 
-var cardOne = cards[0];
-console.log(cards[0]);
-cardsInPlay.push(cardOne);
-console.log("User Flipped ",cardsInPlay);
-var cardTwo = cards[2]; 
-cardsInPlay.push(cardTwo); 
-console.log('User Flipped ', cardsInPlay);
+var cardsInPlay = []; 
 
-if (cardsInPlay.length === 2) {
-	result = "there are two cards";
-} else if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert ("You found a match!"); 
-} else {
-	alert ("Sorry, try again."); 
+function checkForMatch () {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You've found a match!"); 
+	} else {
+		console.log("Sorry, try again!");
+	}
 }
+
+function flipCard (cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch ();
+}
+
+flipCard(0);
+flipCard(2);
 
